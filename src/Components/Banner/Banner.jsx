ee4c9bcc17c0e6f67b2bbc4e84721446c588bb7a
Rakibul Hasan <hassanrakibul926@gmail.com.com>
2024-01-06 22:28:21 +0600

    banner section done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,11 +1,12 @@
 const Banner = () =>{
     return (
-        <div className="bg-red-300 mt-24">
+        <div className="bg-red-300 mt-24 bg-[#f99f1c] flex items-center px-5 rounded-2xl">
             <div>
-                
+                <h1 className="text-4xl font-semibold mb-4 text-white">Deliver Food To Your Door Stepl</h1>
+                <p className="text-white">Authentic Foodl, Quick Service, Fast Delivery</p>
             </div>
             <div>
-
+                <img src="https://raw.githubusercontent.com/0rakib0/Resturent_app/main/src/assets/Image1.png" alt="" />
             </div>
         </div>
     )
